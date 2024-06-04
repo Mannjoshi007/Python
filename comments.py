7ad4b20a3ba6e74de868f6d534dd6396ec365a61
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-06-04 06:19:13 +0530

    Wrinting Comments

---
+++ comments.py
@@ -0,0 +1,4 @@
+# Hi
+# This is the way of giving comments in your code.
+# Comments help you to mark something in the program as per your choice but the ontent of the comments never gets executed . 
+#  By the way it was a multiline comment, if you noticed.
