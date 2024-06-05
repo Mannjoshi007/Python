b72ec2faeb06a38e9696b0cacbc09e3260867f03
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-06-05 08:23:26 +0530

    Types of Datatypes

---
+++ Datatypes.py
@@ -0,0 +1,12 @@
+#types od datatypes with examples
+
+a=1      #Integer
+z=0      #Integer
+k=-12    #Integer
+b=1.2   #Float
+c='karm'    #String
+d="contact"     #String
+e='''working'''     #String
+f=True       #Boolean
+g=False     #Boolean
+h=none      #None
