432ef0a8cf2e321504f28f38fd6725e2f536f20e
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-06-06 07:12:55 +0530

    Typecasting data

---
+++ Typecasting.py
@@ -0,0 +1,12 @@
+# We are learning about typecasting
+# Tyoecasting is conversion of some data/value from datatype to another(desired) one.
+a='12'
+ print(a)    #will print a as a string
+a=int(a)     #converts a from string to an integer
+print(a)     #will print a as an integer
+
+
+
+b=92
+b=str(b)    #makes b a string
+print(b)    #prints b as an string
