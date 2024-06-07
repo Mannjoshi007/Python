9c98d83a44c7e42ea6196b81c808a9aa7d12ca0b
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-06-07 08:05:14 +0530

    Writing Functions

---
+++ functions.py
@@ -0,0 +1,9 @@
+# Functions help us in using the same code on differemt values multipple time sbby not writing it more then once 
+# these make up code short, smartv and easy to interpret
+
+
+def sum(number1,number2):
+    return number1+number2
+
+a=sum(2,6)
+print(a)
