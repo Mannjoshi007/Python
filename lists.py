5fd06b5446db13b02a3f20d4745ebe0b0e9c89cb
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-06-08 09:13:14 +0530

    Making Lists

---
+++ lists.py
@@ -0,0 +1,7 @@
+# Lists are used for mentioning multiple elements in a singl series
+list1=["print","get","work","make","give"]
+list2=[1,2,5,65,498,231,65]
+list3=[1,6,"at",2.3]
+print(list1)
+print(list2)
+print(list3)
