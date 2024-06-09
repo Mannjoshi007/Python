52305960279361f69ef5cea7385e53770aff5b7e
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-06-09 08:35:49 +0530

    Making Tuples

---
+++ Tuples.py
@@ -0,0 +1,13 @@
+# Tuples are the similar version of lists
+# They are able of storing different datatypes in it
+a=(1.3,12.6,9.5)
+b=(1,6,9,4,5)
+c=("get","post","make")
+d=(1.62,12,56,"pass")
+print(a)
+print(b)
+print(c)
+print(d)
+# We can always store a datatype in an another dataypes eg.a list in a tuple or vice - versa
+f=("odour","smell",2,[23,56,2451,44])
+print(f)
