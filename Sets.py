3942a7e7d89665860724d2863202b9a4b941dc19
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-06-10 08:00:59 +0530

    Creating Sets

---
+++ Sets.py
@@ -0,0 +1,11 @@
+# Sets are like tuples but do not alllow repetition of elements within them.
+# Even if we enter repetitive elements only on e copy will we given in output by the set whereever used
+
+a={1,6,33,65,23}
+b={1.2,6.5,5.53,1.5,}
+c={"place","location","working quaters"}
+d={"post",1,56.53,4,61}
+print(a)
+print(b)
+print(c)
+print(d)
