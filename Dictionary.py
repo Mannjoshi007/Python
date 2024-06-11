b149dc018acc0b3940adb2f7398b29f0d0756863
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-06-11 08:21:26 +0530

    Forming Dictionary

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+# Dictionary is used to assign a particular value to a paricular key and this value could be retrieved further from the description of key only
+a={"Book": "Something that provides kknowledge on a partocular topic",
+   "Engineer": "The one who plans to perform particular activiities",
+   "2": "A number",
+   "6.564": "A decimal containing number",
+   "list": [1,6,9,12,412,546]}
+
+
+print(a["Engineer"])
+
