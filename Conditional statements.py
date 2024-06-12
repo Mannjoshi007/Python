bde1170184f598af0bb9f11bea6e4341f874c5cc
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-06-12 08:14:01 +0530

    Conditions based code

---
+++ Conditional statements.py
@@ -0,0 +1,12 @@
+A=int(input("Emter a number : "))
+
+if(A>25):
+    print("You entered a number greater than 25")
+elif(A<25):
+    print("You entered a number lesser than 25")
+else:
+    print("You entered a number equal to 25")  #this prompt will be printed for a number entry equal to 25
+    
+
+
+
