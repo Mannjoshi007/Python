a60c679d12ee95a4bc21a468ff566d27efff4b0b
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-06-16 12:04:53 +0530

    Using Assignment Operators

---
+++ Assignment Operators.py
@@ -0,0 +1,16 @@
+a=int(input("Enter a number 1: "))
+b=int(input("Enter a number 2: "))
+c=int(input("Enter a number 3: "))
+d=int(input("Enter a number 4: "))
+
+a+=6
+print(a)
+
+b-=2
+print(b)
+
+c*=5
+print(c)
+
+d/=10
+print(d)
