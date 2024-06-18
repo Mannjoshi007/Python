0107904f0918e253bedff979202cb19afb67bfcb
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-06-18 07:18:24 +0530

    Using Type() Function

---
+++ Type Function.py
@@ -0,0 +1,15 @@
+# Type() function gives us the datatype of the variable mentioned within the function
+
+a=64
+b=2.65
+c="Work"
+d=[1,6,9,4]
+e=("Location",53,5.5,366)
+f={12,5,6,4,223,84,98}
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
