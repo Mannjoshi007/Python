051159a55d73e793d9f4650be0dd9a8e7d92b4f8
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-06-19 07:37:48 +0530

    Using Input() Function

---
+++ Input Function.py
@@ -0,0 +1,10 @@
+# Input() function works for taking the input from the user's end and this inputten value could be used further
+
+a=input("Enter your favourite number : ")
+b=input("Enter your name : ")
+c=input("Enter your branch : ")
+
+print("The name of the student is ",b)
+print("The student's branch is ",c)
+print("The favourite number of the student is ",a)
+
