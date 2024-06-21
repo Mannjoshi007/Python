2501a91258dc8dd97c249e13ebf8d3f03852b747
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-06-21 09:32:20 +0530

    String Slicing

---
+++ String methods - 2.py
@@ -0,0 +1,14 @@
+String= "This is a string used for demonstartion of string slicing"
+
+print(String[:3])
+
+print(String[0:])
+
+print(String[0:10])
+
+#Strung slicing with skipped letters
+print(String[0:10:2])
+
+print(String[0: :2])
+
+print(String[:15:2])
