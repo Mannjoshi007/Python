410ed5b3dd46bbc77c678aa0c30b36181592b854
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-06-22 07:38:29 +0530

    String functions

---
+++ String methods - 3.py
@@ -0,0 +1,24 @@
+String="Once upon a time in the forests of califonia, there wandered a man named Masakau. The man was on a mission to find some pictues of the extinct birds."
+exp="this is just for experiment"
+
+# Finding String's Length
+print(len(String))
+
+#Verifying String's Ending Words
+print(String.endswith("Excited"))
+print(String.endswith("birds."))
+
+#Counting letter's/Word's occurence in a string
+print(String.count("a"))
+print(String.count("the"))
+
+#Capitalising the first chararter of a string
+print(exp.capitalize())
+
+#Finding th starting index of the mentioned word
+print(String.find("extinct"))
+print(String.find("t"))
+print(String.find("bhbcjkbdkc"))
+
+#Repalcing a word existing in the string by another one
+print(String.replace("man","woman"))
