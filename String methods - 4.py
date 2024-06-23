14f659db824828d35824729d2bcbd6b3909691fe
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-06-23 11:20:12 +0530

    Escape Sequence Characters

---
+++ String methods - 4.py
@@ -0,0 +1,16 @@
+#For new line
+Untitled="This is a string which we will be using for the demonstration of \n escape sequence characters."
+print(Untitled)
+
+#For a tab spacing
+Untitled="This is a string which we will be using for the demonstration of \tescape sequence characters."
+print(Untitled)
+
+#For having a backslash
+Untitled="This is a string which we will be using for the demonstration of \\ escape sequence characters."
+print(Untitled)
+
+#For having a quote
+Untitled="This is a string which we will be using for the demonstration of \' escape sequence characters."
+print(Untitled)
+
