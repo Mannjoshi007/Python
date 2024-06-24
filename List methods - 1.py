e8f63a95977464f1c0c893fafa467776a7b7f9de
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-06-24 06:18:01 +0530

    List Indexing and Slicing

---
+++ List methods - 1.py
@@ -0,0 +1,13 @@
+List1=["work",2,36,3.9546,"indeed","Amar"]
+
+
+# List indexing
+print(List1[1])
+
+#List slicing
+print(List1[0:4])
+print(List1[:5])
+print(List1[0:])
+
+#List slicing with a skip value
+print(List1[0:4:2])
