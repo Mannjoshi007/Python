714f6794b9243501d70ada15c68008c550f0c607
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-06-25 11:11:14 +0530

    Various Methods of a List

---
+++ List methods - 2.py
@@ -0,0 +1,25 @@
+List1=[1,2,6,12,45,86,123,5613,456]
+
+#Sorting the list
+List1.sort()
+print(List1)
+
+#Reversing the list
+List1.reverse()
+print(List1)
+
+#Appending in the list
+List1.append(114)
+print(List1)
+
+#Inserting a value at a fixed mentioned index
+List1.insert(2,46)
+print(List1)
+
+#Deleting the value from a particular index
+List1.pop(5)
+print(List1)
+
+#Removing the value from the list
+List1.remove(45)
+print(List1)
