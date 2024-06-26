f5bb8d7bdd5ade6c5d08dbf47fa8efb52ae8f41d
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-06-26 07:22:42 +0530

    Various Methods for Tuples

---
+++ Tuple Methods.py
@@ -0,0 +1,7 @@
+Tuple1=(1,2,9,6,8,5,4,3,2,8,6,5,6)
+
+#To count the occurences of a value in a tuple
+print(Tuple1.count(6))
+
+#To get the index number for the first occurence of the mentioned element
+print(Tuple1.index(2))
