b370d7807eddf60928c20eb7306da77c1989a9a3
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-06-29 07:49:53 +0530

    Various Set Methods

---
+++ Set Methods - 1.py
@@ -0,0 +1,25 @@
+Set1={1,6,5,8,94,56,48,95}
+Set2={456,996}
+Set3={7,8,45,66}
+
+#To add a number/value to the set
+Set1.add(88)
+print(Set1)
+  #Adding a tuple to the set
+Set1.add((8,9,1))
+print(Set1)
+
+#To print the number of elements of the set
+print(len(Set1))
+
+#To remove a particular element from the set
+Set1.remove(1)
+print(Set1)
+
+#To remove a random number from the set
+Set1.pop()
+print(Set1)
+
+#To clear out the whole set
+Set1.clear()
+print(Set1)
