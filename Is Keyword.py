8bc6091b7c2f5bb727052d29c2836947794d8d83
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-06-30 08:53:31 +0530

    Using Is Keyword

---
+++ Is Keyword.py
@@ -0,0 +1,21 @@
+#Is keyword is used to check the value of the mentioned valriable and give corresponding output
+#Is keyword work for the built-in object values only
+#We need to use == for checking the value of created variales,is won't work over here
+
+
+A=True
+
+if (A is True):
+    print("Ok")
+
+if(A is False):
+    print("Back")
+
+
+B=False
+
+if (B is True):
+    print("Next")
+
+if(B is False):
+    print("Again")
