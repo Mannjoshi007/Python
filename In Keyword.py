c74da8d1bb57eb47b1f08a4196c2ba8578191c4e
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-07-01 08:20:35 +0530

    Using In Keyword

---
+++ In Keyword.py
@@ -0,0 +1,15 @@
+#In keyword checks whether the mentioned value is present in the defined entity(List,Tuple,Set,etc.) and gives the corresponding output in boolean format
+
+List1=[1,9,5,8,6,46,535,6]
+
+print(8 in List1)
+
+print(46 in List1)
+
+print(99 in List1)
+
+print(9 in List1)
+
+print(535 in List1)
+
+print(6 in List1)
