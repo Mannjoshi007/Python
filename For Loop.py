d39c94ab42b3e7beb90856d4656d92b6fe8fcc4e
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-07-04 13:15:04 +0530

    Creating For Loop

---
+++ For Loop.py
@@ -0,0 +1,5 @@
+A=[1,5,6,48,9,5,62323,959,659,95,631,843,846]
+for item in A:
+    print("The item is ",item)
+
+#We can get the iterative values of the iterative datatype by the help of the for loop sequencly(as mentioned)
