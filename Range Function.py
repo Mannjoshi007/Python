283429b64b8848ec6a4f2bb12862dc879f9270df
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-07-05 07:30:52 +0530

    Range Function in For Loop

---
+++ Range Function.py
@@ -0,0 +1,12 @@
+#Range function gives the output of the numbers getting included within the mentioned limits
+#The starting value of the mentioned limit will be included 
+#But not the end limit, only the preceding letter to the mentioned end limit
+
+for i in range(1,10):
+    print (i)
+
+for f in  range(1,11):
+    print(f)
+
+for g in range(-5,-1):
+    print(g)
