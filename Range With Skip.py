9ebca26acc4e33d1a067bf07d028182daf930386
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-07-06 10:43:40 +0530

    Skipping Value Using Range Function

---
+++ Range With Skip.py
@@ -0,0 +1,8 @@
+#We can also skip values to be printed by the Range Function by the use of skip value
+#The third argument within the range function is known as the skpi value
+
+for item in range(1,11,2):
+    print(item)
+
+for it in range(-10,-2,3):
+    print(it)
