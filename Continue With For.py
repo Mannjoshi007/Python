0cb24aeaee037711639ae4ca12cb7428479681f7
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-07-09 08:14:13 +0530

    Using Continue Statement in For Loop

---
+++ Continue With For.py
@@ -0,0 +1,8 @@
+#Continue statement will skip the mentioned value for it and let the program get executed
+for item in range(1,12):
+    
+    if item == 10:
+        continue
+    
+    print(item)
+
