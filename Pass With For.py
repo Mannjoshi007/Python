860adc4f77b9bb16374cb406270eeaa4229b38ee
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-07-10 08:52:40 +0530

    Using Pass Statement in For Loop

---
+++ Pass With For.py
@@ -0,0 +1,12 @@
+#Pass statement is used to give the let off of a command line's following instruction code
+
+A=[1,6,9,4,5,6,461]
+
+for item in A:
+    print(item)
+
+B=[94,2,6,56,31]
+
+for itm in B:
+    pass
+print("Bye")
