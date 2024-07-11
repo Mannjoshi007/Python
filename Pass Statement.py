03c8c364fece7687abbec4d2363e8321c03246bc
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-07-11 07:51:28 +0530

    Using Pass Statement

---
+++ Pass Statement.py
@@ -0,0 +1,23 @@
+#Pass Statement is used to make the no action scenario for a mentioned command line program
+
+for i in range(8):
+    pass
+
+a=12
+
+if a>9:
+    pass
+elif a<25:
+    pass
+elif a==12:
+    pass
+
+
+def sum(n1,n2):
+    pass
+
+num1=2
+num2=5
+
+Total=sum(num1,num2)
+print(Total)
