0422068ae16bdcb4a77d8e3784ad94677dde8b96
Mannjoshi007 <mannpkjoshi@gmail.com>
2024-07-17 12:42:48 +0530

    Application of Loops in Pattern Formation - 2

---
+++ Printing Skip Star Pattern.py
@@ -0,0 +1,17 @@
+n=3
+for i in range(n):
+    print("*",end="")
+print()
+
+for i in range(1,n-1):
+    print("*",end="")
+
+for i in range(1,n-1):
+    print(" ",end="")
+
+for i in range(1,n-1):
+    print("*",end="")
+print()
+
+for i in range(n):
+    print("*",end="")
